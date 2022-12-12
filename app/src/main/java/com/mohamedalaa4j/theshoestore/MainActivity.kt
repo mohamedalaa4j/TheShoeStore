08605e573bcf025655664625f783348218a3e02d
mohamedalaa4j <mohamedalaa4j@gmail.com>
2022-12-12 12:37:27 +0200

    dataBinding enabled & MainActivity layout inflated

--- app/src/main/java/com/mohamedalaa4j/theshoestore/MainActivity.kt
+++ app/src/main/java/com/mohamedalaa4j/theshoestore/MainActivity.kt
@@ -2,10 +2,15 @@ package com.mohamedalaa4j.theshoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.mohamedalaa4j.theshoestore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
     }
 }
