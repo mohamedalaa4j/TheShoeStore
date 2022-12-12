8bd7c453b00b4eea4e229eee456e7c2dc881882a
mohamedalaa4j <mohamedalaa4j@gmail.com>
2022-12-12 14:19:06 +0200

    WelcomeFragment design + App styling done

---
+++ app/src/main/java/com/mohamedalaa4j/theshoestore/WelcomeFragment.kt
@@ -0,0 +1,24 @@
+package com.mohamedalaa4j.theshoestore
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.mohamedalaa4j.theshoestore.databinding.FragmentWelcomeBinding
+
+class WelcomeFragment : Fragment() {
+    private lateinit var binding: FragmentWelcomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_welcome, container, false)
+
+        return binding.root
+    }
+
+
+}
